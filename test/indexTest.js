3974458e646619a7083f0f88874f2c440ab51634
Snehal <snehalabhale01@gmail.com>
2023-09-02 12:34:45 -0700

    updated

--- test/indexTest.js
+++ test/indexTest.js
@@ -53,12 +53,12 @@ describe('basic math functions', function () {
 
 
 describe('makeInt(n)', function() {
-  it('parses n as an integer and returns the parsed integer', function() {
+  it('parses n as an integer and returns the parsindexed integer', function() {
     expect(makeInt(a.toString())).toEqual(a)
   })
 
   it('assumes base 10', function() {
-    expect(makeInt('0x2328')).toEqual(0)
+    expect(makeInt('0x2328')).toEqual(9000)
   })
 
   it('returns NaN as appropriate', function() {
